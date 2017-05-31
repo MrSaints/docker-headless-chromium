386ae6d7c4801fe72f94d0fc378867c1b6fd1f53
Ian Lai <ian@fyianlai.com>
2017-05-31 10:29:00 +0100

    Use a new target (tab) per script execution to avoid race conditions

--- examples/websiteToPDF.js
+++ examples/websiteToPDF.js
@@ -1,23 +1,22 @@
 const CDP = require("chrome-remote-interface");
+
 const options = {
     "host": "chromium",
     "port": "9222"
 };
 
-CDP(options, async (client) => {
-    const {Network, Page} = client;
-
+CDP.New(options, async (err, target) => {
     try {
+        const client = await CDP({target: target});
+        const {Network, Page, Target} = client;
         await Promise.all([Network.enable(), Page.enable()]);
         await Page.navigate({url: "https://www.fyianlai.com"});
         await Page.loadEventFired();
         const result = await Page.printToPDF();
         await process.stdout.write(Buffer.from(result.data, "base64"));
+        await client.close();
+        await CDP.Close(Object.assign(options, {"id": target.id}));
     } catch (err) {
         console.error(err);
     }
-
-    await client.close();
-}).on("error", (err) => {
-    console.error(err);
 });
